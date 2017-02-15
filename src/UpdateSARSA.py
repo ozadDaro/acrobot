2aa6c59ebab0496a3eeabe4d1c4f079c38c787d7
ozad <daro-b13@hotmail.fr>
2017-02-15 12:35:25 +0100

    correction update sarsa

--- src/UpdateSARSA.py
+++ src/UpdateSARSA.py
@@ -1,6 +1,5 @@
-def UpdateSARSA(s, a, r, sp, ap, tab , alpha, gamma ){
+def UpdateSARSA(s, a, r, sp, ap, tab , alpha, gamma ):
 	
 	Q = tab
 	Q(s,a) =  Q(s,a) + alpha * ( r + gamma*Q(sp,ap) - Q(s,a))
 	return Q
-}
